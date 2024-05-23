6696ab43b9bb5ee450a4549d6b80a1ec220e0aad
LisaPaliashchuk <yelizaveta.paliashchuk@Orchestr8.com>
2024-05-23 16:31:08 +0200

    (HW_6.1.1) refactored secret santa e2e test

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://staging.lpitko.ru",
+    baseUrl: 'https://santa-secret.ru/',
     testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
