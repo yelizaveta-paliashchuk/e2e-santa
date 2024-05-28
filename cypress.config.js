c1e34fe67d569ed9e00c14453bbdc9bd0b55cad3
LisaPaliashchuk <yelizaveta.paliashchuk@Orchestr8.com>
2024-05-28 19:40:47 +0200

    installed cucumber plugin

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,21 @@
 const { defineConfig } = require('cypress')
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+const addCucumberPreprocessorPlugin =
+  require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin
+const createEsbuildPlugin =
+  require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://santa-secret.ru/',
     testIsolation: false,
-    chromeBrowserSecurity: false,
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const bundler = createBundler({ plugins: [createEsbuildPlugin(config)] })
+      on('file:preprocessor', bundler)
+      addCucumberPreprocessorPlugin(on, config)
+
+      return config
     },
   },
 })
